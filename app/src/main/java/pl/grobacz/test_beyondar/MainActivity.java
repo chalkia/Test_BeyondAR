f39b10d2a8e2f7ae285ec9124465f8cf41207165
grobacz <grobacz@gmail.com>
2016-03-13 13:04:58 +0100

    Initial commit

---
+++ app/src/main/java/pl/grobacz/test_beyondar/MainActivity.java
@@ -0,0 +1,13 @@
+package pl.grobacz.test_beyondar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
